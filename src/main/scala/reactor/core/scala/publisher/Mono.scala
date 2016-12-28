86a678fc80043dd39fbdbbf61319b1c44cee85d2
Winarto Zhao <winarto@gmail.com>
2016-12-28 22:35:40 +0800

    - Added uploadArchives in build.gradle - Added license header - Added gradle.properties for specifying version

--- src/main/scala/reactor/core/scala/publisher/Mono.scala
+++ src/main/scala/reactor/core/scala/publisher/Mono.scala
@@ -1,3 +1,21 @@
+/*
+* This file is part of Scala wrapper for reactor-core.
+*
+* Scala wrapper for reactor-core is free software: you can redistribute
+* it and/or modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation, either version 3 of the
+* License, or any later version.
+*
+* Foobar is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Scala wrapper for reactor-core.
+* If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+*/
+
 package reactor.core.scala.publisher
 
 import java.time.Duration
