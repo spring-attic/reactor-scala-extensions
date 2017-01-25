d15eda9a7530b7eb199244a4506ddf17aa2eb9fa
Winarto Zhao <winarto@gmail.com>
2017-01-26 00:28:41 +0800

    Added ExecutionContextScheduler

---
+++ src/main/scala/reactor/core/scala/scheduler/ExecutionContextScheduler.scala
@@ -0,0 +1,33 @@
+package reactor.core.scala.scheduler
+
+import java.util.concurrent.Executor
+
+import reactor.core.Cancellation
+import reactor.core.scheduler.{Scheduler, Schedulers}
+import reactor.core.scheduler.Scheduler.Worker
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, ExecutionContextExecutorService}
+
+class ExecutionContextScheduler private(val scheduler: Scheduler) extends Scheduler {
+  override def schedule(task: Runnable): Cancellation = scheduler.schedule(task)
+
+  override def createWorker(): Worker = scheduler.createWorker()
+}
+
+/**
+  * Provide an easy way to create [[Scheduler]] using the provided [[ExecutionContext]] so it can be used like:
+  * <p>
+  * `val mono = Mono.just(1)
+  *             .subscribeOn(ExecutionContextScheduler(executionContext))`
+  */
+object ExecutionContextScheduler {
+  def apply(executionContext: ExecutionContext): ExecutionContextScheduler = {
+    executionContext match {
+      case eces: ExecutionContextExecutorService => new ExecutionContextScheduler(Schedulers.fromExecutorService(eces))
+      case ece: ExecutionContextExecutor => new ExecutionContextScheduler(Schedulers.fromExecutor(ece))
+      case _ => new ExecutionContextScheduler(Schedulers.fromExecutor(new Executor {
+        override def execute(command: Runnable): Unit = executionContext.execute(command)
+      }))
+    }
+  }
+}
