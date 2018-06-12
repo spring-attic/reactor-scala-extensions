ddf6f094027f6a056958ce163a9ef28022e32760
Winarto Zhao <winarto@gmail.com>
2018-06-13 01:09:44 +0800

    Refactored SFlux.min and SFlux.max

--- src/main/scala/reactor/core/scala/publisher/SFluxLike.scala
+++ src/main/scala/reactor/core/scala/publisher/SFluxLike.scala
@@ -28,17 +28,13 @@ trait SFluxLike[T, Self[U] <: SFluxLike[U, Self]] {
       .map(ar => ar.get())
   }
 
-  final def max[R >: T](implicit  ev: Ordering[R]): SMono[Option[R]] = {
-    foldLeft(None: Option[R]){(acc: Option[R], el: T) => {
-      acc.map(a => ev.max(a, el)).orElse(Option(el))
-    }}
-  }
+  final def max[R >: T](implicit  ev: Ordering[R]): SMono[Option[R]] = foldLeft(None: Option[R]){ (acc: Option[R], el: T) => {
+    acc map (a => ev.max(a, el)) orElse Option(el)
+  }}
 
-  final def min[R >: T](implicit  ev: Ordering[R]): SMono[Option[R]] = {
-    foldLeft(None: Option[R]){(acc: Option[R], el: T) => {
-      acc.map(a => ev.min(a, el)).orElse(Option(el))
-    }}
-  }
+  final def min[R >: T](implicit  ev: Ordering[R]): SMono[Option[R]] = foldLeft(None: Option[R]){ (acc: Option[R], el: T) => {
+    acc map (a => ev.min(a, el)) orElse Option(el)
+  }}
 
   final def sum[R >: T](implicit R: Numeric[R]): SMono[R] = {
     import R._
