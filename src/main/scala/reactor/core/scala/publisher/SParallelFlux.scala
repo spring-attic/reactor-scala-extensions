df385a5994a2da91d5d586ea7eb4143713972866
Winarto Zhao <winarto@gmail.com>
2019-03-09 23:05:40 +0800

    Added SParallelFlux.runOn
    Signed-off-by: Winarto Zhao <winarto@gmail.com>

--- src/main/scala/reactor/core/scala/publisher/SParallelFlux.scala
+++ src/main/scala/reactor/core/scala/publisher/SParallelFlux.scala
@@ -3,11 +3,13 @@ package reactor.core.scala.publisher
 import java.util
 import java.util.function.Supplier
 
-import org.reactivestreams.Publisher
+import org.reactivestreams.{Publisher, Subscriber, Subscription}
+import reactor.core.Disposable
 import reactor.core.publisher.{ParallelFlux => JParallelFlux}
+import reactor.core.scheduler.Scheduler
 import reactor.util.concurrent.Queues
 
-class SParallelFlux[T] private(private val jParallelFlux: JParallelFlux[T]) {
+class SParallelFlux[T] private(private val jParallelFlux: JParallelFlux[T]) extends Publisher[T]{
 
   /**
     * Perform a fluent transformation to a value via a converter function which receives
@@ -18,6 +20,68 @@ class SParallelFlux[T] private(private val jParallelFlux: JParallelFlux[T]) {
     * @return the value returned by the converter function
     */
   final def as[U](converter: SParallelFlux[T] => U): U = jParallelFlux.as((t: JParallelFlux[T]) => converter(SParallelFlux(t)))
+
+  /**
+    * Specifies where each 'rail' will observe its incoming values with possibly
+    * work-stealing and a given prefetch amount.
+    * <p>
+    * This operator uses the default prefetch size returned by [[Queues.SMALL_BUFFER_SIZE]].
+    * <p>
+    * The operator will call [[Scheduler.createWorker()]] as many times as this
+    * ParallelFlux's parallelism level is.
+    * <p>
+    * No assumptions are made about the Scheduler's parallelism level, if the Scheduler's
+    * parallelism level is lower than the ParallelFlux's, some rails may end up on
+    * the same thread/worker.
+    * <p>
+    * This operator doesn't require the Scheduler to be trampolining as it does its own
+    * built-in trampolining logic.
+    *
+    * @param scheduler the scheduler to use that rail's worker has run out of work.
+    * @param prefetch  the number of values to request on each 'rail' from the source
+    * @return the new [[SParallelFlux]] instance
+    */
+  final def runOn(scheduler: Scheduler, prefetch: Int = Queues.SMALL_BUFFER_SIZE) = SParallelFlux(jParallelFlux.runOn(scheduler, prefetch))
+
+  /**
+    * Subscribes to this [[SParallelFlux]] by providing an onNext, onError,
+    * onComplete and onSubscribe callback and triggers the execution chain for all
+    * 'rails'.
+    *
+    * @param onNext      consumer of onNext signals
+    * @param onError     consumer of error signal
+    * @param onComplete  callback on completion signal
+    * @param onSubscribe consumer of the subscription signal
+    */
+  final def subscribe(onNext: Option[T => Unit] = None,
+                      onError: Option[Throwable => Unit] = None,
+                      onComplete: Option[() => Unit] = None,
+                      onSubscribe: Option[Subscription => Unit] = None): Disposable = (onNext, onError, onComplete, onSubscribe) match {
+                        case (Some(fn), Some(fe), Some(fc), Some(fs)) => jParallelFlux.subscribe(fn, fe, fc, fs)
+                        case (Some(fn), Some(fe), Some(fc), None) => jParallelFlux.subscribe(fn, fe, fc)
+                        case (Some(fn), Some(fe), None, Some(fs)) => jParallelFlux.subscribe(fn, fe, null, fs)
+                        case (Some(fn), Some(fe), None, None) => jParallelFlux.subscribe(fn, fe)
+                        case (Some(fn), None, Some(fe), Some(fs)) => jParallelFlux.subscribe(fn, null, fe, fs)
+                        case (Some(fn), None, Some(fe), None) => jParallelFlux.subscribe(fn, null, fe, null)
+                        case (Some(fn), None, None, Some(fs)) => jParallelFlux.subscribe(fn, null, null, fs)
+                        case (Some(fn), None, None, None) => jParallelFlux.subscribe(fn)
+                        case (None, Some(fe), Some(fc), Some(fs)) => jParallelFlux.subscribe(null, fe, fc, fs)
+                        case (None, Some(fe), Some(fc), None) => jParallelFlux.subscribe(null, fe, fc)
+                        case (None, Some(fe), None, Some(fs)) => jParallelFlux.subscribe(null, fe, null, fs)
+                        case (None, Some(fe), None, None) => jParallelFlux.subscribe(null, fe)
+                        case (None, None, Some(fc), Some(fs)) => jParallelFlux.subscribe(null, null, fc, fs)
+                        case (None, None, Some(fc), None) => jParallelFlux.subscribe(null, null, fc, null)
+                        case (None, None, None, Some(fs)) => jParallelFlux.subscribe(null, null, null, fs)
+                        case (None, None, None, None) => jParallelFlux.subscribe()
+                      }
+
+  /**
+    * Merge the rails into a [[Flux.sequential]] Flux and
+    * [[Flux#subscribe(Subscriber) subscribe]] to said Flux.
+    *
+    * @param s the subscriber to use on [[#sequential()]] Flux
+    */
+  override def subscribe(s: Subscriber[_ >: T]): Unit = jParallelFlux.subscribe(s)
 
   def asJava: JParallelFlux[T] = jParallelFlux
 }
--- src/test/scala/reactor/core/scala/publisher/SParallelFluxTest.scala
+++ src/test/scala/reactor/core/scala/publisher/SParallelFluxTest.scala
@@ -1,16 +1,32 @@
 package reactor.core.scala.publisher
 
+import org.mockito.Mockito
 import org.scalatest.{FreeSpec, Matchers}
-import reactor.core.publisher.{ParallelFlux => JParallelFlux, Flux => JFlux}
+import reactor.core.publisher.{Flux => JFlux, ParallelFlux => JParallelFlux}
+import reactor.core.scheduler.Schedulers
+import reactor.test.StepVerifier
 
 class SParallelFluxTest extends FreeSpec with Matchers {
   "SParallelFlux" - {
+    val data = Seq(1, 2, 3)
+    val flux = Flux.just(data.head,data.tail:_*)
     ".asJava should convert as Java ParallelFlux" in {
-      Flux.just(1, 2, 3).parallel().asJava shouldBe a[JParallelFlux[Int]]
+      flux.parallel().asJava shouldBe a[JParallelFlux[Int]]
     }
 
     ".apply should convert Java ParallelFlux into SParallelFlux" in {
       SParallelFlux(JFlux.just(1, 2, 3).parallel()).asJava shouldBe a[JParallelFlux[Int]]
+    }
+
+    ".runOn should run on different thread" in {
+      val scheduler = Mockito.spy(Schedulers.parallel())
+      StepVerifier.create(flux.parallel(2).runOn(scheduler))
+        .expectNextMatches(i => data.contains(i))
+        .expectNextMatches(i => data.contains(i))
+        .expectNextMatches(i => data.contains(i))
+        .verifyComplete()
+
+      Mockito.verify(scheduler, Mockito.times(2)).createWorker()
     }
   }
 }
