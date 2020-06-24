99c2289cd586711dd22f91ae1b280894fc43e773
Winarto Zhao <winarto@gmail.com>
2020-06-24 15:58:32 +0800

    Use the "whenRequested" parameter when calling SFlux.error from SFlux.raiseError

--- src/main/scala/reactor/core/scala/publisher/SFlux.scala
+++ src/main/scala/reactor/core/scala/publisher/SFlux.scala
@@ -1168,8 +1168,8 @@ object SFlux {
   def push[T](emitter: FluxSink[T] => Unit, backPressure: FluxSink.OverflowStrategy = OverflowStrategy.BUFFER): SFlux[T] = new ReactiveSFlux[T](JFlux.push(emitter, backPressure))
 
   @deprecated("Use error(Throwable, Boolean) instead")
-  def raiseError[T](exception: Throwable, whenRequested: Boolean = false): SFlux[T] = error(exception)
-  
+  def raiseError[T](exception: Throwable, whenRequested: Boolean = false): SFlux[T] = error(exception, whenRequested)
+
   def error[T](exception: Throwable, whenRequested: Boolean = false): SFlux[T] = new ReactiveSFlux[T](JFlux.error(exception, whenRequested))
 
   def range(start: Int, count: Int): SFlux[Int] = new ReactiveSFlux[Int](JFlux.range(start, count).map((i: java.lang.Integer) => Integer2int(i)))
