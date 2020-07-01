25172bbac644256ceaee214b58ce58c7b7a7364f
Scheidter,Ryan <Ryan.Scheidter@Cerner.com>
2020-07-01 12:57:33 -0500

    Restored invariance of SFlux/SMono.asJava

--- src/main/scala/reactor/core/scala/publisher/SFlux.scala
+++ src/main/scala/reactor/core/scala/publisher/SFlux.scala
@@ -19,6 +19,7 @@ import reactor.util.context.Context
 import reactor.util.function.{Tuple3, Tuple4, Tuple5, Tuple6}
 import reactor.util.retry.Retry
 
+import scala.annotation.unchecked.uncheckedVariance
 import scala.collection.mutable
 import scala.concurrent.duration.Duration
 import scala.concurrent.duration.Duration.Infinite
@@ -82,7 +83,7 @@ trait SFlux[+T] extends SFluxLike[T] with MapablePublisher[T] with ScalaConverte
     coreFlux.as[P]((t: JFlux[_ <: T]) => transformer(SFlux.fromPublisher(t)))
   }
 
-  final def asJava(): JFlux[_ <: T] = coreFlux
+  final def asJava(): JFlux[T @uncheckedVariance] = coreFlux.asInstanceOf[JFlux[T]]
 
   /**
     * Blocks until the upstream signals its first value or completes.
